d67ed8b443164fd1a835cdd4835604acccb4f112
Wolford Li <123850699+WolfordLi@users.noreply.github.com>
2024-08-18 18:27:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 wget --no-check-certificate -O dnsmasq_sniproxy.sh https://raw.githubusercontent.com/myxuchangbin/dnsmasq_sniproxy_install/master/dnsmasq_sniproxy.sh && bash dnsmasq_sniproxy.sh -f
 wget -O nf https://github.com/sjlleo/netflix-verify/releases/download/v3.1.0/nf_linux_amd64 && chmod +x nf && ./nf
-wget -O main.py https://raw.githubusercontent.com/WolfordLi/nfdns/main/main.py
+curl -L -o main.py https://raw.githubusercontent.com/WolfordLi/nfdns/main/main.py
+python3 main.py
